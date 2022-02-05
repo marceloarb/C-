9e5b380cd5f2127d43e32dec707c5024f8c9170a
marceloarb <marceloarthurb@gmail.com>
2022-02-05 12:02:07 -0600

    First C++ programming for programming class.

---
+++ marcelobarbosa-assignment2/marcelobarbosa-assignment2/Circle.cpp
@@ -0,0 +1,18 @@
+//
+//  Circle.cpp
+//  marcelobarbosa-assignment2
+//
+//  Created by Marcelo Barbosa on 2/5/22.
+//
+
+#include <stdio.h>
+
+class Circle
+{
+    public:
+        int calculateCircleArea(int Number){
+            
+            return (Number);
+        }
+    
+};
---
+++ marcelobarbosa-assignment2/marcelobarbosa-assignment2/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  marcelobarbosa-assignment2
+//
+//  Created by Marcelo Barbosa on 2/3/22.
+//
+
+#include <iostream>
+#include "Circle.cpp"
+using namespace std;
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    Circle circle;
+
+    int result;
+    result = circle.calculateCircleArea(20);
+    std::cout << "Hello, World!\n";
+    std::cout << "Hello\n";
+    cout << "Total value is :" << result << endl;
+    return 0;
+}
+
+
+
