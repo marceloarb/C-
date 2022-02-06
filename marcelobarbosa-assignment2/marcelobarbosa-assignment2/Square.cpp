8badedef15ff098ad2b6d624703a3983df6a23c4
Marceloarb <marceloarthurb@gmail.com>
2022-02-06 09:33:20 -0600

    Updating code due to assingment2.

---
+++ marcelobarbosa-assignment2/marcelobarbosa-assignment2/Square.cpp
@@ -0,0 +1,16 @@
+//
+//  Square.cpp
+//  marcelobarbosa-assignment2
+//
+//  Created by Marcelo Barbosa on 2/6/22.
+//
+
+#include <stdio.h>
+
+class Square{
+    public:
+        float calculateSquareArea(float number){
+            float result = number * number;
+            return (result);
+        }
+};
--- marcelobarbosa-assignment2/marcelobarbosa-assignment2/main.cpp
+++ marcelobarbosa-assignment2/marcelobarbosa-assignment2/main.cpp
@@ -6,21 +6,20 @@
 //
 
 #include <iostream>
-#include "Circle.cpp"
+#include "Square.cpp"
 using namespace std;
 
 
 
 int main(int argc, const char * argv[]) {
     
-    Circle circle;
-
-    int result;
-    result = circle.calculateCircleArea(20);
-    std::cout << "Hello, World!\n";
-    std::cout << "Hello\n";
-    cout << "Total value is :" << result << endl;
-    return 0;
+    Square square;
+    float number;
+    float areaSquare;
+    cout << "Enter a Number: ";
+    cin >> number;
+    areaSquare = square.calculateSquareArea(number);
+    cout << "Area of the square is: " << areaSquare << endl;
 }
 
 
