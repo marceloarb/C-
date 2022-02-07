32ec116d8389ec6a769a405ec5960061a9e3186d
Marceloarb <marceloarthurb@gmail.com>
2022-02-07 10:06:28 -0600

    adding more comments

--- marcelobarbosa-assignment2/marcelobarbosa-assignment2/Square.cpp
+++ marcelobarbosa-assignment2/marcelobarbosa-assignment2/Square.cpp
@@ -13,7 +13,11 @@ class Square{
     public:
     //Function or method to calculate object area.
         float calculateSquareArea(float number){
+            
+            //    Declaring variable to store the result
             float result = number * number;
+            
+            //    Returning variable with the result
             return (result);
         }
 };
