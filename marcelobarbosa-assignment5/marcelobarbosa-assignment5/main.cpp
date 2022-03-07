d7d3de71d414829625e03ba96179da3b9beacde4
Marceloarb <marceloarthurb@gmail.com>
2022-03-07 17:48:52 -0600

    Finished assignment 5

--- marcelobarbosa-assignment5/marcelobarbosa-assignment5/main.cpp
+++ marcelobarbosa-assignment5/marcelobarbosa-assignment5/main.cpp
@@ -11,9 +11,10 @@
 
 using namespace std;
 int main(int argc, const char * argv[]) {
+    //Declaring variable
     ifstream inFile;
-    inFile.open("/Users/marcelobarbosa/Desktop/ProgrammingFundamentals1/Assignment5/Data.txt");
     ofstream outFile;
+    ofstream outFileClassStatistic;
     string name;
     float testScore1;
     float testScore2;
@@ -21,59 +22,151 @@ int main(int argc, const char * argv[]) {
     float totalPoints;
     float numericGrade;
     string letterGrade;
+    int numberStudents;
+    float averageNumericGrade;
     
+    //Opening Data file
+    inFile.open("/Users/marcelobarbosa/Desktop/ProgrammingFundamentals1/Assignment5/Data.txt");
+    
+    //Added statement condition to check if the file was open.
     if (!inFile){
         cout << "Cannot open input file. Program terminates!\n"; return 1;
     }
+    
+    //Initializing Output file.
     outFile.open("/Users/marcelobarbosa/Desktop/ProgrammingFundamentals1/Assignment5/Data.out");
-
+    outFileClassStatistic.open("/Users/marcelobarbosa/Desktop/ProgrammingFundamentals1/Assignment5/ClassStatic.out");
+    
+    //Setting output file format
     outFile << fixed << showpoint << setprecision(2);
     
-    inFile >> name;  //read the name
+    //setting output format
+    cout << fixed << showpoint << setprecision(2);
+    
+    //Output the header of the table in the file
+    outFile << left << setw(12) << "Name"
+    << left << setw(12) << "Test1"
+    << left << setw(12) << "Test2"
+    << left << setw(12) << "Test3"
+    << left << setw(12) << "Total Points"
+    << left << setw(12) << "Numeric Grade"
+    << left << setw(12) << "Letter Grade" << endl;
+    
+    //Output the header of the table
+    cout << left << setw(12) << "Name"
+    << left << setw(12) << "Test1"
+    << left << setw(12) << "Test2"
+    << left << setw(12) << "Test3"
+    << left << setw(20) << "Total Points"
+    << left << setw(20) << "Numeric Grade"
+    << left << setw(20) << "Letter Grade" << endl;
+    
+    //Reading the first line from the data file.
+    inFile >> name;
     inFile >> testScore1;
     inFile >> testScore2;
     inFile >> testScore3;
     
+    //Implemented loop to get all the data in the file.
     while(inFile){
-        cout << "oi";
-        totalPoints = testScore1 + testScore2 + testScore3;
-        numericGrade = totalPoints/2; //update sum
-        switch (static_cast<int> (totalPoints) / 3){
-            case 5:
-                letterGrade = 'F';
-                break;
-            case 6:
-                letterGrade = 'D';
-                break;
-            case 7:
-                letterGrade = 'C';
-                break;
-            case 8:
-                letterGrade = 'B';
-                break;
-            case 10: letterGrade = 'A';
-                break;
-            default: cout << "Invalid score." << endl;
-                
         
+        //Calcularing all the tests score from student and assign it to a variable.
+        totalPoints = testScore1 + testScore2 + testScore3;
+        
+        //Calculating the average of the students grade
+        numericGrade = totalPoints/3;
                 
-        outFile << left << setw(12) << name
-        << setw(12) << testScore1
-        << setw(12) << testScore2
-        << setw(12) << testScore3
-        << setw(12) << totalPoints
-        << setw(12) << numericGrade
-        << setw(12) << letterGrade << endl;
-                
-        inFile >> name;  //read the name
+        //Incrementing number of students that exists in the file.
+        numberStudents++;
+        
+        //Calculating the class total grade average.
+        averageNumericGrade = averageNumericGrade + numericGrade;
+        
+        //Implementing statement to determine the letter grade due to the average of the grade
+        switch (static_cast<int> (numericGrade) / 10)
+        {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            letterGrade = 'F';
+            break;
+        case 6:
+            letterGrade = 'D';
+            break;
+        case 7:
+            letterGrade = 'C';
+            break;
+        case 8:
+            letterGrade = 'B';
+            break;
+        case 9:
+        case 10: letterGrade = 'A';
+            break;
+        default:
+            cout << "Invalid score." << endl;
+        }
+        
+        //Output the Student row of the table in the file
+        outFile << right << setw(8) << name
+                << right << setw(8) << testScore1
+                << right << setw(12) << testScore2
+                << right << setw(12) << testScore3
+                << right << setw(20) << totalPoints
+                << right << setw(20) << numericGrade
+                << right << setw(20) << letterGrade << endl;
+        
+        //Output the Student row of the table
+        cout << right << setw(8) << name
+             << right << setw(8) << testScore1
+             << right << setw(12) << testScore2
+             << right << setw(12) << testScore3
+             << right << setw(20) << totalPoints
+             << right << setw(20) << numericGrade
+             << right << setw(20) << letterGrade << endl;
+        
+        //Reading next line from the data file.
+        inFile >> name;
         inFile >> testScore1;
         inFile >> testScore2;
         inFile >> testScore3;
 
-       }
+
     }
-    inFile.close();
+    
+    //Calculating the average grade of the class
+    averageNumericGrade = averageNumericGrade/numberStudents;
+    
+    //Output the header of the table in the file
+    outFileClassStatistic << left << setw(12) <<  "Statistic"
+    << left << setw(12) << "Value" << endl;
+    
+    //Output the next row of table in the file
+    outFileClassStatistic << left << setw(12) << "Number"
+    << right << setw(6) << numberStudents <<  endl;
+    
+    //Output the next row of table in the file
+    outFileClassStatistic << left << setw(12) << "Average"
+    << right << setw(6) << averageNumericGrade << endl;
+
+    ///Output the header of the table
+    cout << left << setw(12) <<  "Statistic"
+    << left << setw(12) << "Value" << endl;
+    
+    //Output the next row of table
+    cout << left << setw(12) << "Number"
+    << right << setw(6) << numberStudents <<  endl;
+    
+    //Output the next row of table
+    cout << left << setw(12) << "Average"
+    << right << setw(6) << averageNumericGrade << endl;
+    
+    //Close files
+    outFileClassStatistic.close();
     outFile.close();
+    inFile.close();
     
     return 0;
 }
